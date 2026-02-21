6f4569df445d9014b0283fe941c16883e701dfdd
Kpv12 <kpvarun12@gmail.com>
2026-02-21 18:00:05 +0530

    Updated status choices and fixed frontend form alignment

--- backend/tickets/serializers.py
+++ backend/tickets/serializers.py
@@ -3,8 +3,6 @@ from .models import Ticket
 
 
 class TicketSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Ticket
         fields = '__all__'
-        read_only_fields = ('id', 'created_at')
